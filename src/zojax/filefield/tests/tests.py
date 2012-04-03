a9135f392c8702f8302ffde4e21f93b5d63908d7
Dmitry Suvorov <suvdim@gmail.com>
2012-04-03 20:30:47 +0400

    #249: fix tests

--- src/zojax/filefield/tests/tests.py
+++ src/zojax/filefield/tests/tests.py
@@ -21,10 +21,49 @@ from copy import deepcopy
 from zope.app.testing import setup
 from zojax.filefield import testing
 
-FileFieldLayer = testing.ZCMLLayer(
-    os.path.join(
-        os.path.dirname(os.path.realpath(__file__)),
-        "ftesting.zcml"), __name__, "FileFieldLayer")
+from zope.app.testing import functional
+from zope.app.component.hooks import setSite
+
+FileFieldLayer = functional.ZCMLLayer(
+    os.path.join(os.path.split(__file__)[0], 'ftesting.zcml'),
+    __name__, 'FileFieldLayer', allow_teardown=True)
+
+def FunctionalDocFileSuite(*paths, **kw):
+    layer = FileFieldLayer
+
+    globs = kw.setdefault('globs', {})
+    globs['http'] = functional.HTTPCaller()
+    globs['getRootFolder'] = functional.getRootFolder
+    globs['sync'] = functional.sync
+    globs['deepcopy'] = deepcopy
+
+    kw['package'] = doctest._normalize_module(kw.get('package'))
+
+    kwsetUp = kw.get('setUp')
+    def setUp(test):
+        functional.FunctionalTestSetup().setUp()
+
+        root = functional.getRootFolder()
+        setSite(root)
+
+    kw['setUp'] = setUp
+
+    kwtearDown = kw.get('tearDown')
+    def tearDown(test):
+        setSite(None)
+        functional.FunctionalTestSetup().tearDown()
+
+    kw['tearDown'] = tearDown
+
+    if 'optionflags' not in kw:
+        old = doctest.set_unittest_reportflags(0)
+        doctest.set_unittest_reportflags(old)
+        kw['optionflags'] = (old|doctest.NORMALIZE_WHITESPACE|doctest.ELLIPSIS)
+
+    suite = doctest.DocFileSuite(*paths, **kw)
+    suite.layer = layer
+    return suite
+
 
 def fromDocFile(path):
     suite = testing.FunctionalDocFileSuite(path)
@@ -42,15 +81,9 @@ def tearDown(test):
 
 def test_suite():
     return unittest.TestSuite((
-        doctest.DocFileSuite(
-            'data.txt', setUp=setUp, tearDown=tearDown,
-            optionflags=doctest.NORMALIZE_WHITESPACE|doctest.ELLIPSIS),
-        doctest.DocFileSuite(
-            'file.txt', setUp=setUp, tearDown=tearDown,
-            optionflags=doctest.NORMALIZE_WHITESPACE|doctest.ELLIPSIS),
-        doctest.DocFileSuite(
-            'image.txt', setUp=setUp, tearDown=tearDown,
-            optionflags=doctest.NORMALIZE_WHITESPACE|doctest.ELLIPSIS),
+        FunctionalDocFileSuite("data.txt"),
+        FunctionalDocFileSuite("file.txt"),
+        FunctionalDocFileSuite("image.txt"),
         fromDocFile('widget.txt'),
         fromDocFile('download.txt'),
         #fromDocFile('configlet.txt'),
